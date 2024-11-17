6c59abf1cb3f67df936837fed845d689f9f3d957
AbdelilahOu <abdelilah4dev@gmail.com>
2024-11-17 08:48:43 +0000

    feat: add post tags

---
+++ graphql/queries/postTag.go
@@ -0,0 +1,19 @@
+package queries
+
+import (
+	"github.com/AbdelilahOu/GoferQl/graphql/resolvers"
+	"github.com/AbdelilahOu/GoferQl/graphql/types"
+
+	"github.com/graphql-go/graphql"
+)
+
+var PostTagsQueries = graphql.Fields{
+	"postTags": &graphql.Field{
+		Type: types.PostTagType,
+		Args: graphql.FieldConfigArgument{
+			"postId": &graphql.ArgumentConfig{Type: graphql.String},
+			"tagId":  &graphql.ArgumentConfig{Type: graphql.String},
+		},
+		Resolve: resolvers.ListPostTags,
+	},
+}
